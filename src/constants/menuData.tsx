1e9d9feb151d74f4daaea47eb124a93ccaa63a3f
ismoilrz <ismoil4766451155@gmail.com>
2026-03-27 14:29:23 +0300

    add logo

--- src/constants/menuData.tsx
+++ src/constants/menuData.tsx
@@ -1,31 +1,29 @@
 import type { MenuData } from "../types/types";
 
 export const menuData: MenuData[] = [
-    {
-        path: "/",
-        name: "home",
-        img: "home.png"
-    },
-    {
-            
-            path: "/members",
-            img: "./public/members.png",
-            name: "members"
-        },
-        {
-            path: "/pos",
-            img: "./public/pos.png",
-            name: "pos"
-        },
-        {
-            path: "/visit-history",
-            img: "./public/visit-history.png",
-            name: "visitHistories"
-        },
-        {
-            path: "/payments",
-            img: "./public/payments.png",
-            name: "payments"
-        }
-    
-]
+  {
+    path: "/",
+    name: "home",
+    img: "/home.png" 
+  },
+  {
+    path: "/members",
+    img: "/members.png", 
+    name: "members"
+  },
+  {
+    path: "/pos",
+    img: "/pos.png", 
+    name: "pos"
+  },
+  {
+    path: "/visit-history",
+    img: "/visit-history.png", 
+    name: "visitHistories"
+  },
+  {
+    path: "/payments",
+    img: "/payments.png", 
+    name: "payments"
+  }
+];
